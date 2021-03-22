9a6efe04ca6c8e3a9718e1e5ed4ddf8dd8a7df7c
aureliacarissaa <18217501@std.stei.itb.ac.id>
2021-03-22 14:03:23 +0700

    remove whitespace and action bar

--- app/src/main/java/com/example/kane/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/kane/ui/home/HomeFragment.java
@@ -4,11 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -46,6 +45,8 @@ public class HomeFragment extends Fragment {
 
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
 
         //init
         database = FirebaseDatabase.getInstance();
